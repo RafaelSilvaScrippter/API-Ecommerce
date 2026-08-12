33361432c1780df8ad12a64e1072d3189f4eaeb9
RafaTheScripter <nativaeleven@gmail.com>
2026-08-12 08:47:28 -0300

    Se o usuário já existir com o email cadastrado retorno o erro

--- api/auth/tablesAuth.ts
+++ api/auth/tablesAuth.ts
@@ -4,12 +4,12 @@ export const TableAuth = /*SQL */ `
     CREATE TABLE IF NOT EXISTS "users" (
         "id" INTEGER PRIMARY KEY,
         "name" TEXT NOT NULL,
-        "email" TEXT NOT NULL COLLATE NOCASE,
+        "email" TEXT NOT NULL COLLATE NOCASE UNIQUE,
         "password" TEXT NOT NULL,
         "cep" TEXT,
         "cidade" TEXT,
         "estado" TEXT,
         "numero" TEXT,
         "rua" TEXT
-    )
+    ) STRICT;
 `
